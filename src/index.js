3c504d4b5aa4ab414b307694bf40acf8fff2f949
Felipe Tun <felipe.tun.cauich@gmail.com>
2023-01-12 13:02:52 -0600

    test

--- src/index.js
+++ src/index.js
@@ -6,5 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/whatsapp', apiRoute);
+app.use('/', (req, res) => {
+    return res.json({ message: "Api WhatsApp!"})
+});
 
 app.listen(PORT, () => console.log('Running in port ' +  PORT))
