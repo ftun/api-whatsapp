218ebb4ec5719bef1f733fb4a34b58ef26366858
Felipe Tun <felipe.tun.cauich@gmail.com>
2023-01-13 13:23:00 -0600

    add public folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const apiRoute = require('./routes/routes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// app.use(express.static('public'));
+// app.use(express.json());
 app.use('/whatsapp', apiRoute);
+app.use('/ping', (req, res) => {
+    res.sendFile(path.join(__dirname + './../public/index.html'));
+});
 app.use('/', (req, res) => {
     return res.json({ message: "Api WhatsApp!"})
 });
