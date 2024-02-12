1b445ce5ebb916e1572d28d9d2dee7de2d907f82
Felipe Tun <felipe.tun.cauich@gmail.com>
2024-02-12 13:34:31 -0600

    assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const apiRoute = require('./routes/routes');
 const path = require('path');
+const fs = require('fs')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const assetlinks = fs.readFileSync(path.join(__dirname + './../public/.well-known/assetlinks.json'))
 
 // app.use(express.static('public'));
 // app.use(express.json());
@@ -11,6 +14,10 @@ app.use('/whatsapp', apiRoute);
 app.use('/ping', (req, res) => {
     res.sendFile(path.join(__dirname + './../public/index.html'));
 });
+app.use("/.well-known/assetlinks.json", (req, res) => {
+    res.set('Content-Type', 'application/json')
+    return res.status(200).send(assetlinks)
+})
 app.use('/', (req, res) => {
     return res.json({ message: "Api WhatsApp!"})
 });
